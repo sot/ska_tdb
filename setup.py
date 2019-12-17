3862562f988aa287e80d19faff3e8897149a1cda
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from setuptools import setup
 
-from Ska.tdb import __version__
-
-
 try:
     from testr.setup_helper import cmdclass
 except ImportError:
@@ -15,7 +12,8 @@ setup(name='Ska.tdb',
       url="http://cxc.harvard.edu/mta/ASPECT/tool_doc/pydocs/Ska.tdb",
       description='Access to Chandra Telemetry Database (TDB)',
       author_email='taldcroft@cfa.harvard.edu',
-      version=__version__,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       zip_safe=False,
       packages=['Ska', 'Ska.tdb', 'Ska.tdb.tests'],
       tests_require=['pytest'],
